69381a7e07f32676d18c9dc262c24fbc44e1a1a6
Wilgner Souza <wilgner.showtime@gmail.com>
2024-03-13 17:25:04 -0400

    feat: add animation scrollreveal

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ScrollReveal from "scrollreveal";
 import { saveForm } from "../scripts/firebase";
 
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 1500,
+  delay: 200,
+});
+
 const Main = () => {
+  useEffect(() => {
+    sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text", {});
+    sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
+      delay: 400,
+    });
+    sr.reveal(".home__social-icon", { interval: 200 });
+    sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
+  }, []);
   const handleSubmit = (event) => {
-    console.log('está chamando');
-    event.preventDefault(); 
+    console.log("está chamando");
+    event.preventDefault();
     saveForm();
   };
   return (
@@ -57,7 +73,7 @@ const Main = () => {
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
           >
-            <mask id="mask0" maskType="alpha">
+            <mask id="mask0" mask-type="alpha">
               <path d="M9.19024 145.964C34.0253 76.5814 114.865 54.7299 184.111 29.4823C245.804 6.98884 311.86 -14.9503 370.735 14.143C431.207 44.026 467.948 107.508 477.191 174.311C485.897 237.229 454.931 294.377 416.506 344.954C373.74 401.245 326.068 462.801 255.442 466.189C179.416 469.835 111.552 422.137 65.1576 361.805C17.4835 299.81 -17.1617 219.583 9.19024 145.964Z" />
             </mask>
             <g mask="url(#mask0)">
@@ -218,7 +234,12 @@ const Main = () => {
       <section className="contact section" id="contact">
         <h2 className="section-title">Contato</h2>
         <div className="contact__container bd-grid">
-          <form onSubmit={handleSubmit} action="" method="post" className="contact__form">
+          <form
+            onSubmit={handleSubmit}
+            action=""
+            method="post"
+            className="contact__form"
+          >
             <input
               type="text"
               autoComplete="name"
