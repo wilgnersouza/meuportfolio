654fb85f12384ded5520b84d875d4a366d2798ba
Wilgner Souza <wilgner.showtime@gmail.com>
2024-03-14 18:37:17 -0400

    fix: eslint config and typical animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './style/style.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
+import React from "react";
+import "./style/style.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React from "react";
+import Typical from "react-typical";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="footer__title">Wilgner</p>
-      <div className="footer__social">
-        <a href="https://www.facebook.com/wilgner.souza.stw" target="_blank" className="footer__icon" rel="noreferrer"><i className='bx bxl-facebook'></i></a>
-        <a href="https://www.instagram.com/showtimewill97/" target="_blank" className="footer__icon" rel="noreferrer"><i className='bx bxl-instagram'></i></a>
+      <div className="footer__title">
+        <p>{""}</p>
+        <Typical
+          loop={Infinity}
+          steps={["Wilgner", 1200, "Wilgner Souza", 1200, ]}
+        ></Typical>
       </div>
-      <p className="footer__copy">Copyright &#169; 2024 - All rights reserved</p>
+
+      <div className="footer__social">
+        <a
+          href="https://www.facebook.com/wilgner.souza.stw"
+          target="_blank"
+          className="footer__icon"
+          rel="noreferrer"
+        >
+          <i className="bx bxl-facebook"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/showtimewill97/"
+          target="_blank"
+          className="footer__icon"
+          rel="noreferrer"
+        >
+          <i className="bx bxl-instagram"></i>
+        </a>
+      </div>
+      <p className="footer__copy">
+        Copyright &#169; 2024 - All rights reserved
+      </p>
     </footer>
   );
-}
+};
 
 export default Footer;
