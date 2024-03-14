b1f1ee6482cc2dfcd5489e91f7b3b342f67e81fa
Wilgner Souza <wilgner.showtime@gmail.com>
2024-03-14 17:25:04 -0400

    feat: add note disclaimer in scroll reveal animation

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,8 +16,8 @@ const sr = ScrollReveal({
 const Main = () => {
   useEffect(() => {
     sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text", {});
-    sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
-      delay: 400,
+    sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img, .disclaimer-note", {
+      delay: 300,
     });
     sr.reveal(".home__social-icon", { interval: 200 });
     sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
